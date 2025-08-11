92407d9fbac656eab1ee8892657fa127b6825a3d
shuvamb1 <shuvambhattacharya896@gmail.com>
2025-08-11 21:56:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -62,5 +62,6 @@ app.get("/:short_code", async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.listen(5000, () => console.log("🚀 Server running on http://localhost:5000"));
